7e5a388d4d05a8154cf3a9106847afd992e4160b
Vitor Henrique <vhenriquebm@gmail.com>
2022-01-05 13:50:29 -0300

    primeira versão do xylophone

--- xylophone/ViewController.swift
+++ xylophone/ViewController.swift
@@ -6,14 +6,118 @@
 //
 
 import UIKit
+import AVFoundation //é necessário importar o AVFoundation para fazer os sons funcionarem
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+   
+    @IBAction func botaoC(_ sender: Any) {
+        
+        playSoundC()
+    }
+    
+    
+    @IBAction func botaoD(_ sender: Any) {
+        
+        playSoundD()
+        
+    }
+    
+    
+    @IBAction func botaoE(_ sender: Any) {
+        
+        playSoundE()
+    }
+    
+    
+    @IBAction func botaoF(_ sender: Any) {
+        
+        playSoundF()
+    }
+    
+    
+    @IBAction func botaoG(_ sender: Any) {
+        playSoundG()
+    }
+    
+    
+    @IBAction func botaoA(_ sender: Any) {
+        
+        playSoundA()
+    }
+    
+    
+    @IBAction func botaoB(_ sender: Any) {
+        
+        playSoundB()
+    }
+    
+    
+    //funcões que irão chamar os sons ao clicar no botão
+    
+    
+    func playSoundC() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+}
+    
+    
+    func playSoundD() {
+        let url = Bundle.main.url(forResource: "D", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+}
+    
+    func playSoundE() {
+        let url = Bundle.main.url(forResource: "E", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+}
+    
+    
+    func playSoundF() {
+        let url = Bundle.main.url(forResource: "F", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+}
+    
+    
+    
+    func playSoundG() {
+        let url = Bundle.main.url(forResource: "G", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+}
+    
+    
+    
+    func playSoundA() {
+        let url = Bundle.main.url(forResource: "A", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+}
+    
+    
+    func playSoundB() {
+        let url = Bundle.main.url(forResource: "B", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+}
+    
 
 }
+
+
+
+
+
+
 
